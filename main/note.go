bcd0aa0bf6c61a88919f983abedad4130806c254
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2024-12-28 17:18:26 -0600

    added the combinations of having 3 valves

--- main/note.go
+++ main/note.go
@@ -1,11 +1,67 @@
+components {
+  id: "note"
+  component: "/main/note.script"
+}
 embedded_components {
-  id: "sprite"
+  id: "sprite1"
   type: "sprite"
   data: "default_animation: \"logo\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 192.0\n"
+  "  y: 192.0\n"
+  "}\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/main/main.atlas\"\n"
   "}\n"
   ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "default_animation: \"logo\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 192.0\n"
+  "  y: 192.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 80.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
+embedded_components {
+  id: "sprite3"
+  type: "sprite"
+  data: "default_animation: \"logo\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 192.0\n"
+  "  y: 192.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 160.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+  }
 }
